9388a209dc094e60f002268c23e3ef8f99a18875
Lesquivemeau <waterfoxyt@gmail.com>
2020-04-22 14:25:19 +0200

    ✨ Working prototype !

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 async function parseQuestionsAsync() {
     return new Promise(resolve => {
-        qAndA = []
 
-        json = $.getJSON('/res.json', (res) => {
-            qAndA = [];
-    
-            res.data.questions.forEach(element => {
-                qAndA = [...qAndA, {
-                    "id": element.id,
-                    "question": atob(element.question),
-                    "enonce": atob(element.enonce),
-                }]
-            });
-            
-            qAndA.forEach(element => {
-                solution = res.data.solutions.find(x => x.idQuestion == element.id);
-    
-                element.choix = [];
-    
-                for (let [k, v] of Object.entries(solution.choix)) {
-                    element.choix.push(atob(v));
-                }
-                element.remediation = atob(solution.remediation);
-            });
-        }).then(() => {
-            resolve(qAndA);
-        })
+        document.getElementById("frame").innerHTML = "";
+
+        qAndA = [];
+        input = JSON.parse(document.getElementById("json-input").value);
+
+        input.data.questions.forEach(element => {
+            qAndA = [...qAndA, {
+                "id": element.id,
+                "question": Base64.decode(element.question),
+                "enonce": Base64.decode(element.enonce),
+            }]
+        });
+
+        qAndA.forEach(element => {
+            solution = input.data.solutions.find(x => x.idQuestion == element.id);
+
+            element.choix = [];
+
+            for (let [k, v] of Object.entries(solution.choix)) {
+                element.choix.push(Base64.decode(v));
+            }
+            element.remediation = Base64.decode(solution.remediation);
+        });
+
+        resolve(qAndA);
     })
+}
+
+function getHtmlList(a) {
+    str = ""
+    a.forEach((elem) => {
+        str = str + ("<li>" + elem + "</li>")
+    })
+
+    return ("<ul>" + str + "</ul>")
+}
+
+async function displayQuestionsAsync() {
+    const questions = await parseQuestionsAsync();
+    innerDiv = document.getElementById("frame");
+    questions.forEach(question => {
+        innerDiv.insertAdjacentHTML('beforeend', `
+        <div class="questionBox">
+                <h2>${question.question}</h2>
+                ${question.enonce}
+            <div class="questionSolutions">
+                <h3>Réponses: </h3>
+                ${getHtmlList(question.choix)}
+            </div>
+        <div>
+        `)
+    });
+
 }
